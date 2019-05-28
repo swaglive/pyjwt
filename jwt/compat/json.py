282ee4ae8ffdd552c3d15daaf0d5f8f1581d5509
Clyde Wu <claudwu@gmail.com>
2019-05-28 19:59:37 +0800

    Prevent kwargs for json loads

--- jwt/compat/json.py
+++ jwt/compat/json.py
@@ -21,22 +21,15 @@ def dumps(obj, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan
         uuid_mode=None, allow_nan=allow_nan)
 
 
-def loads(*args, **kwargs):
-    kwargs.pop('encoding', None)
-    if kwargs.get('cls', None):
-        cls_kwargs = copy.copy(kwargs)
-        cls_kwargs.pop('cls', None)
-        kwargs['object_hook'] = kwargs['cls'](**cls_kwargs).object_hook
-    
-    kwargs.pop('parse_float', None)
-    kwargs.pop('parse_int', None)
-    kwargs.pop('parse_constant', None)
-    kwargs.pop('object_pairs_hook', None)
-    kwargs.update({
-        'allow_nan': False,
-        'number_mode': NM_NATIVE,
-        'datetime_mode': DM_ISO8601 | DM_SHIFT_TO_UTC,
-    })
+def loads(string, encoding=None, cls=None, object_hook=None, parse_float=None, parse_int=None,
+        parse_constant=None, object_pairs_hook=None, strict=True):
+    # In order to prevent kwargs but still allow give the value of 'strict'
+    # Add a additional arg 'strict' with default value.
 
-    return _loads(*args, **kwargs)
+    return _loads(string,
+        object_hook=object_hook if not cls else cls(
+            object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
+            parse_constant=parse_constant, strict=strict, object_pairs_hook=object_pairs_hook).object_hook,
+        number_mode=NM_NATIVE, datetime_mode=DM_ISO8601 | DM_SHIFT_TO_UTC,
+        uuid_mode=None, parse_mode=None, allow_nan=False)
 
